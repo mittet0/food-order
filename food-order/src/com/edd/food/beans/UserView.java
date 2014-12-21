ad47f50a5e50ce21dfcff4a7b7dc5d25e7621906
mittet0 <gangov.dimitar@gmail.com>
2014-12-21 17:07:15 +0200

    Initial index and login pages

---
+++ food-order/src/com/edd/food/beans/UserView.java
@@ -0,0 +1,31 @@
+package com.edd.food.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class UserView {
+
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void save() {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Welcome " + username + " " + password));
+    }
+}
